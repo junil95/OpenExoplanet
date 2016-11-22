13d10559dd3e0101a9e93ad4a5e5201bd3027345
John Song <John.Song.Toronto@gmail.com>
2016-11-22 17:20:16 -0500

    Changed pom

--- src/main/java/com/team23/oec_app/OECMain.java
+++ src/main/java/com/team23/oec_app/OECMain.java
@@ -29,7 +29,8 @@ public class OECMain extends HttpServlet
     	}
     	else if(req.getRequestURI().contains("css")){
     		resp.setContentType("text/css");
-    		resp.getWriter().print(readFile(req.getRequestURI(), StandardCharsets.UTF_8));
+
+    		resp.getWriter().print(readFile("css/style.min.css", StandardCharsets.UTF_8));
         	resp.getWriter().close();
     	}
     	else if(req.getRequestURI().contains("js")){

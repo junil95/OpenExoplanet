0f67578322d589b2da03be6ba2aed2a946ca90cf
John Song <John.Song.Toronto@gmail.com>
2016-11-22 17:38:57 -0500

    Changed pom

--- src/main/java/com/team23/oec_app/OECMain.java
+++ src/main/java/com/team23/oec_app/OECMain.java
@@ -39,7 +39,6 @@ public class OECMain extends HttpServlet
     		resp.getWriter().close();
     	}
     	else if(req.getRequestURI().contains("woff")){
-    		resp.setContentType("application/font-woff");
     		resp.getWriter().print(readFile(req.getRequestURI().substring(1), StandardCharsets.UTF_8));
     		resp.getWriter().close();
     	}

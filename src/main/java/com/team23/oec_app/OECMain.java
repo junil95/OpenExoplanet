81b01ca9060438a971c521b94136f0ddfc4241aa
John Song <John.Song.Toronto@gmail.com>
2016-11-23 01:59:45 -0500

    Changed pom

--- src/main/java/com/team23/oec_app/OECMain.java
+++ src/main/java/com/team23/oec_app/OECMain.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,9 +16,19 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import com.google.gson.Gson;
+import com.team23.Driver;
+import com.team23.ModelStarSystems.Systems;
+import com.team23.UpdateTools.Merge;
+import com.team23.UpdateTools.UpdateStorage;
+import com.team23.UpdateTools.generateXML;
+
 public class OECMain extends HttpServlet
 {
 
+	/**
+	 * Default method used to handle all GET requests given to the server.
+	 */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -26,6 +37,11 @@ public class OECMain extends HttpServlet
     		resp.setContentType("text/html");
     		resp.getWriter().print(readFile("index.html", StandardCharsets.UTF_8));
         	resp.getWriter().close();
+        	
+        	// Doing the initial merge and setting up local repos
+        	if(!Driver.isInitialMergeDone()){
+        		Driver.initialSetupOrResetLocalCopies();
+        	}
     	}
     	else if(req.getRequestURI().contains("css")){
     		resp.setContentType("text/css");
@@ -47,23 +63,56 @@ public class OECMain extends HttpServlet
     		resp.getWriter().close();
     	}
     	else if (req.getRequestURI().equals("/update")){
+    		// Calling from Driver to get all the new updated celestial objects
+        	if(!Driver.isInitialMergeDone()){
+        		Driver.initialSetupOrResetLocalCopies();
+        	}
+        	
+        	// Feteching initial updates
+    		Driver.detectInitialUpdates();
     		
+    		ArrayList<String> list = new ArrayList<String>();
     		
+    		list.add(Driver.getNewSystems());
+    		list.add(Driver.getNewPlanetConflicts());
     		
+    		list.add(Driver.getNewPlanets());
+    		list.add(Driver.getNewPlanetConflicts());
     		
+    		list.add(Driver.getNewStars());
+    		list.add(Driver.getNewStarConflicts());
     		
+    		// Writing back data
+    		Gson gson = new Gson();	
+    		resp.getWriter().print(gson.toJson(list));
+    		
+    		resp.getWriter().print(readFile(req.getRequestURI().substring(1), StandardCharsets.UTF_8));
     		resp.getWriter().close();
     	}
     }
     
+    /**
+     * Default method used to handle all POST requests given to the server.
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-    	if (req.getRequestURI().equals("/commit")){
+    	if (req.getRequestURI().equals("/upload")){
+    		resp.getWriter().close();
+    	}
+    	else if (req.getRequestURI().equals("/upload")){
+        	Driver.executeMerge();
     		resp.getWriter().close();
     	}
     }
     
-    static String readFile(String path, Charset encoding) 
+    /**
+     * Helper function used to convert all contents of a file to a single string.
+     * @param path Path of the file
+     * @param encoding The encoding format of the file
+     * @return A String with all contents of the file
+     * @throws IOException If the file doesn't exist
+     */
+    public static String readFile(String path, Charset encoding) 
     		  throws IOException 
     		{
     		  byte[] encoded = Files.readAllBytes(Paths.get(path));

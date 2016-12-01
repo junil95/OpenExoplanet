40ce8505110ac00575d29a79420edd5b219d6035
John Song <John.Song.Toronto@gmail.com>
2016-11-30 23:22:56 -0500

    Changed pom

--- js/exoplanet.js
+++ js/exoplanet.js
@@ -331,9 +331,11 @@ function commitChanges(){
   result.push(exportAsJSON(seperateFunctions("existingConflictingStar")));
   result.push(exportAsJSON(seperateFunctions("existingConflictingPlanet")));
 
+  console.log(JSON.stringify(result));
+
   // Sending it as a post
   $.post("https://pacific-shelf-92985.herokuapp.com/setkey", {key: key}, function(text) {
-    $.post("https://pacific-shelf-92985.herokuapp.com/upload", {result: result}, function(data) {
+    $.post("https://pacific-shelf-92985.herokuapp.com/upload", {result: JSON.stringify(result)}, function(data) {
       window.alert("Your data has been sent");
     });
   });
